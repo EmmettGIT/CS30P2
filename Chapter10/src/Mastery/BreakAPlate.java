d12d6a3b98a6c142170d5ff59a4cd6a348237c40
49254008 <49254008@L815WD220009.edu.cbe.ab.ca>
2026-09-24 10:02:07 -0600

    10:01am

---
+++ Chapter10/src/Mastery/BreakAPlate.java
@@ -0,0 +1,66 @@
+package Mastery;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Font;
+
+public class BreakAPlate {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					BreakAPlate window = new BreakAPlate();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public BreakAPlate() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 352);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JPanel panel = new JPanel();
+		frame.getContentPane().add(panel, BorderLayout.CENTER);
+		panel.setLayout(null);
+		
+		JButton PlayButton = new JButton("Play");
+		PlayButton.setFont(new Font("Tahoma", Font.BOLD, 20));
+		PlayButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		PlayButton.setBounds(147, 146, 142, 56);
+		panel.add(PlayButton);
+		
+		JLabel Plates = new JLabel("");
+		Plates.setBounds(10, 11, 414, 108);
+		panel.add(Plates);
+	}
+}
